40f50a31df12a91a3693322a91f7e692e5c6be16
JT Dotzler <jtdotz22gmail.com>
2019-11-18 18:02:42 -0700

    First commit to github; siginificant work on dialogue system

---
+++ Assets/Scripts/Dialogue/Option.cs
@@ -0,0 +1,18 @@
+﻿namespace RPGKit.Dialogue
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    public class Option : MonoBehaviour
+    {
+        public Text displayText;
+
+        public void SetText(string text)
+        {
+            displayText.text = text;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Dialogue/Subtitles.cs
@@ -0,0 +1,33 @@
+﻿namespace RPGKit.Dialogue {
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    public class Subtitles : MonoBehaviour
+    {
+        public Text text;
+
+        public Color nameColor;
+        public Color speechColor;
+
+        public void DisplayLine(Line line)
+        {
+            text.text = string.Format("<color=#{0}>{1}: </color><color=#{2}>{3}</color>",
+                ColorUtility.ToHtmlStringRGBA(nameColor),
+                line.speaker.GetName(),
+                ColorUtility.ToHtmlStringRGBA(speechColor),
+                line.text
+             );
+        }
+
+        public void Clear()
+        {
+            text.text = "";
+        }
+
+        public void Start()
+        {
+            Clear();
+            //DisplayLine(new Line(new Speaker("Tester"), "Hello, world!"));
+        }
+    }
+}
